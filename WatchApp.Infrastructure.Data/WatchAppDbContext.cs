25b868618ff1de6481aa5acc8204e7b2a246e548
Ingvar Friðbjörnsson <ingvarfr@gmail.com>
2018-10-08 15:08:36 +0200

    Dbcontext

--- WatchApp.Infrastructure.Data/WatchAppDbContext.cs
+++ WatchApp.Infrastructure.Data/WatchAppDbContext.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace WatchApp.Infrastructure.Data
 {
-    class WatchAppDbContext
+    public class WatchAppDbContext: DbContext
     {
+        public DbSet<WatchesRepository> Watches { get; set; }
+
+        public WatchAppDbContext(DbContextOptions<WatchAppDbContext> opt): base(opt)
+        { }
     }
 }
